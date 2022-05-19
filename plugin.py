8d37d4e684e14e8a0ee424ea9c2e505452aeac88
Rafal Chlodnicki <rchl2k@gmail.com>
2022-05-19 21:55:01 +0200

    minor lint fixes

--- plugin.py
+++ plugin.py
@@ -7,10 +7,12 @@ from urllib.parse import urlparse
 import weakref
 
 from LSP.plugin.core.protocol import Request, Location
-from LSP.plugin.core.typing import Any, Callable, List, Mapping, Optional, Tuple, Union
+from LSP.plugin.core.sessions import Session
+from LSP.plugin.core.typing import Any, Callable, Dict, List, Mapping, Optional, Tuple, Union
 from LSP.plugin.core.registry import LspTextCommand
 from LSP.plugin import register_plugin, unregister_plugin
 from lsp_utils import NpmClientHandler
+
 
 class LspGrammarlyCommand(LspTextCommand):
     session_name = 'grammarly'
@@ -22,6 +24,7 @@ class LspGrammarlyCommand(LspTextCommand):
     def message_dialog(self, msg: str) -> None:
         sublime.message_dialog(self.msg_prefix + msg)
 
+
 class LspGrammarlyExecuteLoginCommand(LspGrammarlyCommand):
     cmd = "$/getOAuthUrl"
     cmd_callback = "$/handleOAuthCallbackUri"
@@ -29,35 +32,42 @@ class LspGrammarlyExecuteLoginCommand(LspGrammarlyCommand):
     internal_redirect_uri = "vscode://znck.grammarly/auth/callback"
     external_redirect_uri = internal_redirect_uri
     redirect_uri = internal_redirect_uri
+    weaksession = None  # type: Optional[weakref.ref[Session]]
 
     def run(self, edit: sublime.Edit) -> None:
         def run_async() -> None:
             session = self.session_by_name()
+            if not session:
+                return
             self.weaksession = weakref.ref(session)
-            if session:
-                request = Request(self.cmd, self.redirect_uri, None, progress=True)
-                session.send_request(request, self._handle_response,
-                    lambda r: self.error_message("Failed to start login process"))
+            request = Request(self.cmd, self.redirect_uri, None, progress=True)
+            session.send_request(request, self._handle_response_async,
+                                 lambda _: self.error_message("Failed to start login process"))
         sublime.set_timeout_async(run_async)
 
-    def _handle_response(self, response: Union[List[Location], None]) -> None:
-        link = re.sub("state=[^&]*", "state=" + base64.b64encode(bytes(self.external_redirect_uri, 'utf-8')).decode('ascii'), str(response))
-        session = self.weaksession()
+    def _handle_response_async(self, response: Union[List[Location], None]) -> None:
+        link = re.sub("state=[^&]*",
+                      "state=" + base64.b64encode(bytes(self.external_redirect_uri, 'utf-8')).decode('ascii'),
+                      str(response))
+        session = self.weaksession and self.weaksession()
         if session:
-            session.window.show_input_panel("Open this url in the browser and paste in its place the redirection:", link,
-                                     lambda r: self._send_response(r), None, lambda: self._send_response(None))
+            session.window.show_input_panel(
+                "Open this url in the browser and paste in its place the redirection:", link,
+                lambda r: self._send_response_async(r), None, lambda: self._send_response_async(None))
 
-    def _send_response(self, input: Optional[str]) -> None:
+    def _send_response_async(self, input: Optional[str]) -> None:
         if input:
             res = urlparse(input)
             if res.scheme != 'vscode':
                 self.error_message("Unexpected authorization URL schema. Hint: the inputted authorization URL should start with 'vscode://'")
                 return
             request = Request(self.cmd_callback, input, None, progress=True)
-            session = self.weaksession()
+            session = self.weaksession and self.weaksession()
             if session:
-                session.send_request(request, lambda p: None,
-                    lambda r: self.error_message("Invalid authorization-response url. Did it start with 'vscode://'?"))
+                session.send_request(
+                    request, lambda p: None,
+                    lambda _: self.error_message("Invalid authorization-response url. Did it start with 'vscode://'?"))
+
 
 class LspGrammarlyExecuteLogoutCommand(LspGrammarlyCommand):
     cmd = "$/logout"
@@ -67,15 +77,15 @@ class LspGrammarlyExecuteLogoutCommand(LspGrammarlyCommand):
             session = self.session_by_name()
             assert(session)
             request = Request(self.cmd, None, None, progress=True)
-            session.send_request(request, self._handle_response,
-                lambda r: self.error_message("Failed to logout"))
+            session.send_request(request, self._handle_response_async, lambda r: self.error_message("Failed to logout"))
         sublime.set_timeout_async(run_async)
 
-    def _handle_response(self, response: Union[List[Location], None]) -> None:
+    def _handle_response_async(self, response: Union[List[Location], None]) -> None:
         if response:
             self.message_dialog("Logout response: " + str(response))
         else:
             self.message_dialog("Logged out")
+
 
 class LspGrammarlyExecuteIsConnectedCommand(LspGrammarlyCommand):
     cmd = "$/isUserAccountConnected"
@@ -86,32 +96,31 @@ class LspGrammarlyExecuteIsConnectedCommand(LspGrammarlyCommand):
             if not session:
                 return
             request = Request(self.cmd, None, None, progress=True)
-            session.send_request(request, self._handle_response,
-                lambda r: self.error_message("Failed to query whether connected: " + str(r)))
+            session.send_request(request, self._handle_response_async,
+                                 lambda r: self.error_message("Failed to query whether connected: " + str(r)))
         sublime.set_timeout_async(run_async)
 
-    def _handle_response(self, response: Union[List[Location], None]) -> None:
+    def _handle_response_async(self, response: Union[List[Location], None]) -> None:
         if response:
             self.message_dialog("Connected: " + str(response))
         else:
             self.message_dialog("Not connected")
 
+
 class LspGrammarlyPlugin(NpmClientHandler):
     package_name = "LSP-Grammarly"
     server_directory = "grammarly-languageserver"
-    server_binary_path = os.path.join(
-        server_directory, "node_modules", "grammarly-languageserver", "bin", "server.js"
-    )
+    server_binary_path = os.path.join(server_directory, "node_modules", "grammarly-languageserver", "bin", "server.js")
 
     @classmethod
     def name(cls) -> str:
         return LspGrammarlyCommand.session_name
-    
+
     @classmethod
     def minimum_node_version(cls) -> Tuple[int, int, int]:
         return (16, 13, 0)
 
-    def m___onDocumentStatus(self, params):
+    def m___onDocumentStatus(self, params: Dict[str, str]) -> None:
         if "uri" not in params or "status" not in params:
             return
         session = self.weaksession()
@@ -131,14 +140,17 @@ class LspGrammarlyPlugin(NpmClientHandler):
         if command["command"] == "grammarly.dismiss" and "arguments" in command:
             dismissals = command["arguments"]
             session = self.weaksession()
+            assert(session)
             request = Request("$/dismissSuggestion", [dismissals], None, progress=True)
             session.send_request(request, lambda p: None)
             done_callback()
             return True
         return super().on_pre_server_command(command, done_callback)
 
+
 def plugin_loaded() -> None:
     register_plugin(LspGrammarlyPlugin)
+
 
 def plugin_unloaded() -> None:
     unregister_plugin(LspGrammarlyPlugin)
